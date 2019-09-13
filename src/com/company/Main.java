e058bb79f37c82164bb8f2fa59a4e833449e394b
Jason Bishop <jasonbishop11@gmail.com>
2019-09-13 12:11:03 -0400

    Random Numbers Answers

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,12 +3,15 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("GoodBye");
+	    System.out.println("GoodBye");
 
-	//double x = Math.random();
-	int x = (int)(Math.random()*10+10);
-	//(int)(Math.random()*a+b) //numbers starting at b
+	    int x = (int)(Math.random()*10);                              //1
+	    //int x = (int)(Math.random()*6+1);                             //2
+        //int x = (int)(Math.random()*2);                               //3
+        //int x = (int)(Math.random()*6+1+(int)(Math.random()*6+1));    //4
+        //int x = (int)(Math.random()*35+65);                           //5
+	    //double x = (double)(Math.random()*10);                        //6
 
-	System.out.println(x);
+	    System.out.println(x);
     }
 }
