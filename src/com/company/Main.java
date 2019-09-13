944d40d6859781f23bbd044e45e339e541fdac25
Jason Bishop <jasonbishop11@gmail.com>
2019-09-13 11:22:54 -0400

    Math.random() examples

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
 	System.out.println("GoodBye");
+
+	//double x = Math.random();
+	int x = (int)(Math.random()*10+10);
+	//(int)(Math.random()*a+b) //numbers starting at b
+
+	System.out.println(x);
     }
 }
