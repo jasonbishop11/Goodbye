a604283d692df50d4db231d776bcf107fd552349
Jason Bishop <jasonbishop11@gmail.com>
2019-09-13 12:20:53 -0400

    Random Numbers Answers Corrected

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,12 +5,12 @@ public class Main {
     public static void main(String[] args) {
 	    System.out.println("GoodBye");
 
-	    int x = (int)(Math.random()*10);                              //1
+	    //int x = (int)(Math.random()*11);                              //1
 	    //int x = (int)(Math.random()*6+1);                             //2
         //int x = (int)(Math.random()*2);                               //3
         //int x = (int)(Math.random()*6+1+(int)(Math.random()*6+1));    //4
-        //int x = (int)(Math.random()*35+65);                           //5
-	    //double x = (double)(Math.random()*10);                        //6
+        //int x = (int)(Math.random()*36+65);                           //5
+	    double x = (double)(Math.random()*9);                        //6
 
 	    System.out.println(x);
     }
